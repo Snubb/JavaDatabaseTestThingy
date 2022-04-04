be99f9f007f4a73ddccbcaae2dfb572d3583b1b6
Snubb <60389050+Snubb@users.noreply.github.com>
2022-04-04 09:38:01 +0200

    Initial commit

---
+++ src/DatabaseConnectorTest.java
@@ -0,0 +1,8 @@
+public class DatabaseConnectorTest {
+    public static void main(String[] args) {
+        DatabaseConnector dbconn = new DatabaseConnector();
+        String databaseContent = dbconn.getDatabaseContent();
+        System.out.println(databaseContent);
+        //System.out.println(dbconn.getTableInfo());
+    }
+}
